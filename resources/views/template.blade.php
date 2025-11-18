4740382b80b25f5522c1135c8ad420f008a3dbdc
Nathanaël <nathanael.leang@gmail.com>
2025-11-18 09:18:32 +0100

    ajout template et index propore

---
+++ resources/views/template.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Album Photo - Accueil</title>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+    
+    <body>
+        <header>Super Film</header>
+        <nav>
+            <a href="/">Accueil</a>
+            <a href="/albums">Albums</a>
+            <a href="/photos">Photos</a>
+            <a href="/tags">Tags</a>
+            <a href="/ajoutPhoto">Ajout Photo</a>
+        </nav>
+        
+        
+        <main>
+            @yield("contenu")
+        </main>
+    </body>
+</html>
