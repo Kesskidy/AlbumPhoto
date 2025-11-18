d8eca97772e7a65e715ff9a418370cc10538ea8f
YanderStudio <lariaric6@gmail.com>
2025-11-18 09:15:36 +0100

    template and style.css added

---
+++ resources/views/template.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Album photo</title>
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+    <div class="nav">
+        <nav>
+            <a href="/">Accueil</a>
+            <a href="/">Albums</a>
+            <a href="/">Photos</a>
+            <a href="/">Login</a>
+            <a href="/">Register</a>
+        </nav>
+    </div>
+
+    <main>
+        <div>
+            @yield("contenu")
+        </div>
+    </main>
+</body>
+
